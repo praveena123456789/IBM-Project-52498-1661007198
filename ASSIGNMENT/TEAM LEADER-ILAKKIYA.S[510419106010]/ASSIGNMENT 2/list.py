b579f3e0ef1e3b213cdbb87352e10407a1893d68
ILAKKKIYA123S <113825244+ILAKKKIYA123S@users.noreply.github.com>
2022-10-01 20:40:19 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM LEADER-ILAKKIYA.S[510419106010]/ASSIGNMENT 2/list.py
@@ -0,0 +1,21 @@
+list=['0','2','4','3']
+#insert an interger
+list.insert(1,'1')
+print(list)
+#print the list
+print(list)
+#remove the list
+list.remove('0')
+print(list)
+#append
+list.append('5')
+print(list)
+#sort the list
+list.sort()
+print(list)
+#pop the list
+print("The original list is: "+str(list))
+print("The last element using pop() is :"+str(list.pop()))
+#reverse the list
+list.reverse()
+print("reversed list:",list)
---
+++ ASSIGNMENT/TEAM LEADER-ILAKKIYA.S[510419106010]/ASSIGNMENT 2/reverse and slice.py
@@ -0,0 +1,4 @@
+a=input("Enter the string:")
+output=a[::-1]
+print(output)
+print(a[:4])
